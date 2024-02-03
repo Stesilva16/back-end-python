cbdbce258d7f2eb4a8d8e3af17c0869b83de8fd7
Stesilva16 <stefany.silva2020.16@gmail.com>
2024-02-03 17:31:15 -0300

    exercicios resolvidos

--- orientacao-a-objetos/7-classes-abstratas/lista-de-exercicios-2.py
+++ orientacao-a-objetos/7-classes-abstratas/lista-de-exercicios-2.py
@@ -12,3 +12,57 @@
 # 5. Clientes homens por enquanto não têm direito a cheque especial.
 
 # Para modelar seu sistema, utilize obrigatoriamente os conceitos "classe", "herança", "propriedades", "encapsulamento" e "classe abstrata".
+
+from abc import ABC, abstractmethod
+
+# Classe abstrata representando um cliente
+class Cliente(ABC):
+    def __init__(self, nome, telefone, renda_mensal):
+        self.nome = nome
+        self.telefone = telefone
+        self.renda_mensal = renda_mensal
+    
+    @abstractmethod
+    def obter_cheque_especial(self):
+        pass
+# Subclasse de Cliente para clientes mulheres    
+class ClientMulher(Cliente):
+    def obter_cheque_especial(self):
+        return -self.renda_mensal
+    
+# Subclasse de Cliente para clientes homens    
+class ClienteHomen(Cliente):
+    def obter_cheque_especial(self):
+        return 0
+    
+# Classe representando uma conta corrente    
+class ContaCorrente:
+    def __init__(self, clientes):
+        self.clientes = clientes
+        self.saldo = 0
+        self.operacoes = []
+    
+    # Método para realizar depósito na conta    
+    def depositar(self,valor):
+        self.saldo += valor
+        self.operacoes.append(f'Depósito de R$ {valor}')
+        
+     # Método para realizar saque na conta, considerando o cheque especial para clientes mulheres 
+    def sacar(self, valor):
+        # Verifica se o saldo pós-saque não ultrapassa o cheque especial disponível para qualquer cliente
+        if self.saldo - valor >= min(cliente.obter_cheque_especial() for cliente in self.clientes):
+            self.saldo -=valor
+            self.operacoes.append(f'Saque de R${valor}')
+        else:
+            print('Saque não permitido. Saldo insuficiente.')
+            
+#Exemplo de uso:
+cliente_mulher = ClientMulher("Mary", "123456845", 5000)
+cliente_homem = ClienteHomen("Juan", "987564622", 6000)
+
+conta = ContaCorrente([cliente_mulher])
+conta.depositar(1000)
+conta.sacar(3000) #Saque permitido devido ao cheque especial
+
+print(f'Saldo atual: R${conta.saldo}')
+print('Operações:', conta.operacoes)
--- orientacao-a-objetos/9-erros_e_excecoes/lista-de-exercicios-3.py
+++ orientacao-a-objetos/9-erros_e_excecoes/lista-de-exercicios-3.py
@@ -2,19 +2,20 @@
 ## No entanto, ele não está funcionando bem. Você pode consertá-lo?
 
 def calcular_media(valores):
-    tamanho = 1
-    soma = 0.0
-    for i, valor in enumerate(valores):
-        soma += valor
-        i += 1
+    tamanho = len(valores)
+    soma = sum(valores)
     media = soma / tamanho
+    return media
 
 continuar = True
 valores = []
+
 while continuar:
     valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor:')
     if valor.lower() == 'ok':
-        continuar = false
+        continuar = False
+    else:
+        valores.append(float(valor)) #adicionar os valores na lista
 
 media = calcular_media(valores)
-print('A média calculada para os valores {} foi de {}'.format(valores, media))
+print(f'A média calculada para os valores {valores} foi de {media}.')
